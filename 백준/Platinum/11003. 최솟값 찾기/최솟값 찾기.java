a6373ff4198410f2bbcc08ff115428d93243ecf7
AHJ <121944891+HyungJun-An@users.noreply.github.com>
2025-04-07 16:54:34 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2256 ms, Memory: 615364 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    //최소값 찾기
+    public static void main(String[] args)throws IOException{
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st= new StringTokenizer(bf.readLine());
+        int n=Integer.parseInt(st.nextToken());
+        int l=Integer.parseInt(st.nextToken());
+        st=new StringTokenizer(bf.readLine());
+        Deque<int []> q=new ArrayDeque<>();
+        StringBuffer sb = new StringBuffer();
+        for(int i=0; i<n; i++) {
+            int num=Integer.parseInt(st.nextToken());
+            while(!q.isEmpty()&&q.peekLast()[0]>num) q.pollLast();  //q배열이 비어있지 않거나 q배열 첫번째가 num보다 크면
+            //q배열 마지막 값을 삭제하라
+
+            q.offer(new int[]{num ,i});	//q배열에 인덱스와 num을 저장하라
+
+            if(q.peekFirst()[1]<i-(l-1)) { //첫번째 값이 윈도우를 벗어났을
+                q.pollFirst();
+
+            }
+            sb.append(q.peekFirst()[0] +" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
