352725ae7a13366643fe4c952f7d68a21a98799c
AHJ <121944891+HyungJun-An@users.noreply.github.com>
2025-04-09 09:59:01 +0900

    Update 최솟값 찾기.java

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -4,7 +4,6 @@ public class Main {
     //최소값 찾기
     public static void main(String[] args)throws IOException{
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st= new StringTokenizer(bf.readLine());
         int n=Integer.parseInt(st.nextToken());
         int l=Integer.parseInt(st.nextToken());
@@ -13,8 +12,9 @@ public class Main {
         StringBuffer sb = new StringBuffer();
         for(int i=0; i<n; i++) {
             int num=Integer.parseInt(st.nextToken());
-            while(!q.isEmpty()&&q.peekLast()[0]>num) q.pollLast();  //q배열이 비어있지 않거나 q배열 첫번째가 num보다 크면
-            //q배열 마지막 값을 삭제하라
+            while(!q.isEmpty()&&q.peekLast()[0]>num) 
+                q.pollLast();  //q배열이 비어있지 않거나 q배열 첫번째가 num보다 크면
+                               //q배열 마지막 값을 삭제하라
 
             q.offer(new int[]{num ,i});	//q배열에 인덱스와 num을 저장하라
 
