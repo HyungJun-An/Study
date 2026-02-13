2be1571190919da92cfaa21ee51ec6fd7ca8cf72
AHJ <121944891+HyungJun-An@users.noreply.github.com>
2026-02-13 18:56:40 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 15816 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,32 +1,37 @@
+/*
+    백준2750
+    1. 아이디어
+        버블정렬로 풀자
+    2. 시간복잡도
+        O(N^2) ==> 1,000 * 1,000 = 1,000,000
+
+    3. 자료구조
+        배열저장 int[]
+ */
 import java.io.*;
-import java.util.Arrays;
+
+
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(br.readLine());
-        String[] sanswer = new String[count];
-        String temp= "";
-        int[] answer = new int[count];
-        for(int i=0; i<count; i++) {
+        int N = Integer.parseInt(br.readLine());
+        int[] num = new int[N];
+        for(int i=0; i<N; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
 
-            temp = br.readLine();
-
-            //중복 제거
-            if(Arrays.asList(sanswer).contains(temp)) {
-                continue;
+        for(int j=0; j<N; j++) {
+            for(int i=0; i<N-1; i++) {
+                if(num[i] > num[i+1]) {
+                    int tmp = num[i+1];
+                    num[i+1] = num[i];
+                    num[i] = tmp;
+                }
             }
-            sanswer[i] = temp;
-
-
         }
-        for(int i=0; i<sanswer.length; i ++){
-            answer[i] = Integer.parseInt(sanswer[i]);
-        }
-        Arrays.sort(answer);
-
-        for(int num: answer) {
-            System.out.print(num + "\n");
+        for(int i=0; i<N; i++) {
+            System.out.println(num[i]);
         }
     }
 }
