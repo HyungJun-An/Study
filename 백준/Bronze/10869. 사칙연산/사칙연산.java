af1cef1c975d97676dc40ee72722b5b13806c51c
AHJ <121944891+HyungJun-An@users.noreply.github.com>
2026-03-07 23:58:31 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,25 @@
+/*
+    백준 10869
+    1. 아이디어
+
+    2. 시간복잡도
+
+    3. 자료구조
+
+*/
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
